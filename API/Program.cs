459e970bfec1950e62a9cc49e3b970656b426548
pravindesai0790 <pravindesai0790@gmail.com>
2024-02-08 16:45:30 +0530

    Added product related seed data

--- API/Program.cs
+++ API/Program.cs
@@ -38,6 +38,7 @@ var logger = services.GetRequiredService<ILogger<Program>>();
 try
 {
     await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
 }
 catch (Exception ex)
 {
