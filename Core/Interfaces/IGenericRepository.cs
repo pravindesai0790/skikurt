7a1cd8980deff32fac71e99e15d53c74161e3d47
pravindesai0790 <pravindesai0790@gmail.com>
2024-02-13 18:12:25 +0530

    Added SpecificationEvaluator class and implemented repo with spec methods

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -1,10 +1,16 @@
 using Core.Entities;
+using Core.Specifications;
 
 namespace Core.Interfaces
 {
     public interface IGenericRepository<T> where T : BaseEntity
     {
+        //Generic pattern methods
         Task<T> GetByIdAsync(int id);
         Task<IReadOnlyList<T>> ListAllAsync();
+
+        //Specification pattern methods
+        Task<T> GetEntityWithSpec(ISpecification<T> spec);
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
     }
 }
