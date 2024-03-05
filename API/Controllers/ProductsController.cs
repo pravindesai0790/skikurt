5cb8aa6355f5217720df5f484aff9e6f67b707bc
pravindesai0790 <pravindesai0790@gmail.com>
2024-03-05 16:33:18 +0530

    Added pagination for product

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -27,9 +27,9 @@ namespace API.Controllers
 
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(
-            string sort, int? brandId, int? typeId) 
+            [FromQuery]ProductSpecParams productParams) 
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
 
             var products = await _productRepo.ListAsync(spec);
 
