b6adc003bc1341f2e69327a247beadf0f8a0dce8
pravindesai0790 <pravindesai0790@gmail.com>
2024-03-13 19:15:51 +0530

    Added sort functionality

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -13,11 +13,12 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(brandId?: number, typeId?: number) {
+  getProducts(brandId?: number, typeId?: number, sort?: string) {
     let params = new HttpParams();
 
     if(brandId) params = params.append('brandId', brandId);
     if(typeId) params = params.append('typeId', typeId);
+    if(sort) params = params.append('sort', sort);
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', { params });
   }
