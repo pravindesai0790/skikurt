9f11f0f3b751f9c8f3a27e4b74e55e1ea040b3bc
pravindesai0790 <pravindesai0790@gmail.com>
2024-03-14 14:22:11 +0530

    Added error controller and component for testing the error responses

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'test-error', component: TestErrorComponent },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    TestErrorComponent
   ],
   imports: [
     CommonModule,
