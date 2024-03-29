bc0dd8f0271c1daf7145fff49ed92f58c7479db2
pravindesai0790 <pravindesai0790@gmail.com>
2024-03-29 18:16:55 +0530

    Config xng-breadcrumb pkg in application

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, data: {breadcrumb: 'Home'} },
   { path: 'test-error', component: TestErrorComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: 'server-error', component: ServerErrorComponent },
-  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
+  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
