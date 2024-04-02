30eea712598c267584462221008e6ed27941eec4
pravindesai0790 <pravindesai0790@gmail.com>
2024-04-02 21:22:11 +0530

    Added new IdentityDbContext and extention method and done required configurations

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,18 @@
+using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppIdentityDbContext>(opt => 
+            {
+                opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
+            });
+
+            return services;
+        }
+    }
+}
