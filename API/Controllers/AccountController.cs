475d1bb8e1305e68dfc6506514186c7ccbf31f90
pravindesai0790 <pravindesai0790@gmail.com>
2024-04-03 18:06:05 +0530

    Created account controller and login method and new required Dtos

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+using API.Dtos;
+using API.Errors;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        public UserManager<AppUser> _userManager { get; }
+        public SignInManager<AppUser> _signInManager { get; }
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if(user == null) return Unauthorized(new ApiResponse(401));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if(!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return new UserDto 
+            {
+                Email = user.Email,
+                DisplayName = user.DisplayName,
+                Token = "This will be a toen"
+            };
+        }
+    }
+}
