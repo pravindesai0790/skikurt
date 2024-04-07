bf9c57f3fde3fc4ca2ccf6ee106dbff2bd27adac
pravindesai0790 <pravindesai0790@gmail.com>
2024-04-07 18:05:26 +0530

    Created the order migration

--- Infrastructure/Data/Config/OrderConfiguration.cs
+++ Infrastructure/Data/Config/OrderConfiguration.cs
@@ -12,6 +12,7 @@ namespace Infrastructure.Data.Config
             {
                 a.WithOwner();
             });
+            builder.Navigation(a => a.ShipToAddress).IsRequired();
             builder.Property(s => s.Status)
                 .HasConversion(
                     o => o.ToString(),
