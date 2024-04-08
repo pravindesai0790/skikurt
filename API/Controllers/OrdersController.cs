9fd58a6cf7d18911ffde9f6497aeafe74b098f78
pravindesai0790 <pravindesai0790@gmail.com>
2024-04-08 23:11:16 +0530

    Implemented order get methods in order controller

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -21,6 +21,34 @@ namespace API.Controllers
             _orderService = orderService;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Order>>> GetOrdersForUser()
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+
+            var orders = await _orderService.GetOrdersForUserAsync(email);
+
+            return Ok(orders);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrderByIdForUser(int id)
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+
+            var order = await _orderService.GetOrderByIdAsync(id, email);
+
+            if(order == null) return NotFound(new ApiResponse(404));
+
+            return order;
+        }
+
+        [HttpGet("deliveryMethods")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+        {
+            return Ok(await _orderService.GetDeliveryMethodsAsync());
+        }
+
         [HttpPost]
         public async Task<ActionResult<Order>> CreateOrder(OrderDto orderDto)
         {
