f01f7622ca62fa9bc05aa162f4164a0876deaa3b
pravindesai0790 <pravindesai0790@gmail.com>
2024-04-08 19:51:37 +0530

    Created the order controller and createorder post method

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -19,6 +19,8 @@ namespace API.Helpers
             CreateMap<CustomerBasketDto, CustomerBasket>();
 
             CreateMap<BasketItemDto, BasketItem>();
+
+            CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
         }
     }
 }
