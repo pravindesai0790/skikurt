cdb1d9f32d7ec83c8881f1aecf32a0dd4f6213cd
pravindesai0790 <pravindesai0790@gmail.com>
2024-04-09 16:40:57 +0530

    Created the address form

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-address',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout-address.component.scss']
 })
 export class CheckoutAddressComponent {
-
+  @Input() checkoutForm?: FormGroup;
 }
