837b661ae8e3545f4fea961033e3d8b0dd85ff74
pravindesai0790 <pravindesai0790@gmail.com>
2024-05-02 13:53:13 +0530

    Populating and saving of the user address functionality added in checkout form

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    this.getAddressFormValue();
+  }
 
   checkoutForm = this.fb.group({
     addressForm: this.fb.group({
@@ -17,7 +22,7 @@ export class CheckoutComponent {
       street: ['', Validators.required],
       city: ['', Validators.required],
       state: ['', Validators.required],
-      zipcode: ['', Validators.required]
+      zipCode: ['', Validators.required]
     }),
     deliveryForm: this.fb.group({
       deliveryMethod: ['', Validators.required]
@@ -26,4 +31,12 @@ export class CheckoutComponent {
       nameOnCard: ['', Validators.required]
     })
   })
+
+  getAddressFormValue() {
+    this.accountService.getUserAddress().subscribe({
+      next: address => {
+        address && this.checkoutForm.get('addressForm')?.patchValue(address)
+      }
+    })
+  }
 }
