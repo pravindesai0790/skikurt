4505e0d5ac23514a6567c2eb8aabb97163e5e9e9
pravindesai0790 <pravindesai0790@gmail.com>
2024-05-08 20:19:38 +0530

    Created the orders list in orders component

--- client/src/app/orders/orders.service.ts
+++ client/src/app/orders/orders.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Order } from '../shared/models/order';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getOrdersForUser() {
+    return this.http.get<Order[]>(this.baseUrl + 'orders')
+  }
+
+  getOrderDetailed(id: number) {
+    return this.http.get<Order>(this.baseUrl + 'orders/' + id);
+  }
 }
