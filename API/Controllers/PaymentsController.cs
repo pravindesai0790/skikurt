5364b0d0ac2d537359534397c79f346e8f90390d
pravindesai0790 <pravindesai0790@gmail.com>
2024-06-02 18:04:05 +0530

    Update payment service to capture stripe webhook apis

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
 {
     public class PaymentsController : BaseApiController
     {
-        private const string WhSecret = "";
+        private const string WhSecret = "whsec_e9369eded83197880b0fac79b3c44d2a1da0afa2d2eafd64ff479320be544664";
         private readonly IPaymentService _paymentService;
         private readonly ILogger<PaymentsController> _logger;
         public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
@@ -30,7 +30,7 @@ namespace API.Controllers
             return basket;
         }
 
-        [HttpPost]
+        [HttpPost("webhook")]
         public async Task<ActionResult> StripeWebhook()
         {
             var json = await new StreamReader(Request.Body).ReadToEndAsync();
@@ -38,19 +38,21 @@ namespace API.Controllers
             var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], WhSecret);
 
             PaymentIntent intent;
-            //Order order;
+            Order order;
 
             switch (stripeEvent.Type)
             {
                 case "payment_intent.succeeded":
                     intent = (PaymentIntent) stripeEvent.Data.Object;
                     _logger.LogInformation("Payment succeeded: ", intent.Id);
-                    // TODO: update the order with the new status
+                    order = await _paymentService.UpdateOrderPaymentSucceeded(intent.Id);
+                    _logger.LogInformation("Order updated to payment received: ", order.Id);
                     break;
                 case "payment_intent.payment_failed":
                     intent = (PaymentIntent) stripeEvent.Data.Object;
                     _logger.LogInformation("Payment failed: ", intent.Id);
-                    // TODO: update the order with the new status
+                    order = await _paymentService.UpdateOrderPaymentFailed(intent.Id);
+                    _logger.LogInformation("Order updated to payment failed: ", order.Id);
                     break;
             }
 
--- Core/Interfaces/IPaymentService.cs
+++ Core/Interfaces/IPaymentService.cs
@@ -1,9 +1,12 @@
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 
 namespace Core.Interfaces
 {
     public interface IPaymentService
     {
         Task<CustomerBasket> CreateOrUpdatePaymentIntent(string backetId);
+        Task<Order> UpdateOrderPaymentSucceeded(string paymentIntentId);
+        Task<Order> UpdateOrderPaymentFailed(string paymentIntentId);
     }
 }
