d90026a2d7aa23ba075e3e41768b693f0d06231d
pravindesai0790 <pravindesai0790@gmail.com>
2024-06-02 16:36:49 +0530

    Created StripeWebhook API

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -1,16 +1,21 @@
 using API.Errors;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace API.Controllers
 {
     public class PaymentsController : BaseApiController
     {
+        private const string WhSecret = "";
         private readonly IPaymentService _paymentService;
-        public PaymentsController(IPaymentService paymentService)
+        private readonly ILogger<PaymentsController> _logger;
+        public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
         {
+            _logger = logger;
             _paymentService = paymentService;
         }
 
@@ -23,6 +28,33 @@ namespace API.Controllers
             if(basket == null) return BadRequest(new ApiResponse(400, "Problem with your basket"));
 
             return basket;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], WhSecret);
+
+            PaymentIntent intent;
+            //Order order;
+
+            switch (stripeEvent.Type)
+            {
+                case "payment_intent.succeeded":
+                    intent = (PaymentIntent) stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment succeeded: ", intent.Id);
+                    // TODO: update the order with the new status
+                    break;
+                case "payment_intent.payment_failed":
+                    intent = (PaymentIntent) stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment failed: ", intent.Id);
+                    // TODO: update the order with the new status
+                    break;
+            }
+
+            return new EmptyResult();
         }
     }
 }
