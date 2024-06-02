4bd549425f9ab29a8055e46357616225c7d47024
pravindesai0790 <pravindesai0790@gmail.com>
2024-06-02 15:32:01 +0530

    Stripe validation for local validation for enable/disable submit order button

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -24,6 +24,9 @@ export class CheckoutPaymentComponent implements OnInit {
   cardNumber?: StripeCardNumberElement;
   cardExpiry?: StripeCardExpiryElement;
   cardCvc?: StripeCardCvcElement;
+  cardNumberComplete = false;
+  cardExpiryComplete = false;
+  cardCvcComplete = false;
   cardErrors: any;
   loading = false;
 
@@ -38,6 +41,7 @@ export class CheckoutPaymentComponent implements OnInit {
         this.cardNumber = elements.create('cardNumber');
         this.cardNumber.mount(this.cardNumberElement?.nativeElement);
         this.cardNumber.on('change', event => {
+          this.cardNumberComplete = event.complete;
           if(event.error) this.cardErrors = event.error.message;
           else this.cardErrors = null;
         })
@@ -45,6 +49,7 @@ export class CheckoutPaymentComponent implements OnInit {
         this.cardExpiry = elements.create('cardExpiry');
         this.cardExpiry.mount(this.cardExpiryElement?.nativeElement);
         this.cardExpiry.on('change', event => {
+          this.cardExpiryComplete = event.complete;
           if(event.error) this.cardErrors = event.error.message;
           else this.cardErrors = null;
         })
@@ -52,11 +57,19 @@ export class CheckoutPaymentComponent implements OnInit {
         this.cardCvc = elements.create('cardCvc');
         this.cardCvc.mount(this.cardCvcElement?.nativeElement);
         this.cardCvc.on('change', event => {
+          this.cardCvcComplete = event.complete;
           if(event.error) this.cardErrors = event.error.message;
           else this.cardErrors = null;
         })
       }
     })
+  }
+
+  get paymentFormComplete() {
+    return this.checkoutForm?.get('paymentForm')?.valid
+      && this.cardNumberComplete
+      && this.cardExpiryComplete
+      && this.cardCvcComplete;
   }
   
   async submitOrder() {
