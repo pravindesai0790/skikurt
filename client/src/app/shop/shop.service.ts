fdd5cd765fba6e3423de98e9b17253e4b6c5c308
pravindesai0790 <pravindesai0790@gmail.com>
2024-06-05 23:22:04 +0530

    Caching client side data for product catalogs in shop page

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,43 +5,66 @@ import { Product } from '../shared/models/product';
 import { Brand } from '../shared/models/brand';
 import { Type } from '../shared/models/type';
 import { ShopParams } from '../shared/models/shopParams';
-import { map, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
-  products: Product[] =[];
   brands: Brand[] = [];
   types: Type[] = [];
+  pagination?: Pagination<Product[]>;
+  shopParams = new ShopParams();
+  productCache = new Map<string, Pagination<Product[]>>();
 
   constructor(private http: HttpClient) { }
 
   getProduct(id: number) {
-    const product = this.products.find(p => p.id === id);
-
-    if(product) return of(product);
+    const product = [...this.productCache.values()]
+      .reduce((acc, paginatedResult) => {
+        return {...acc, ...paginatedResult.data.find(x => x.id === id)}
+      }, {} as Product);
+    
+    if(Object.keys(product).length !== 0) return of(product);
     
     return this.http.get<Product>(this.baseUrl + 'products/' + id);
   }
 
-  getProducts(shopParams: ShopParams) {
+  getProducts(useCache = true): Observable<Pagination<Product[]>> {
+    if(!useCache) this.productCache = new Map();
+
+    if(this.productCache.size > 0 && useCache) {
+      if(this.productCache.has(Object.values(this.shopParams).join('_'))) {
+        this.pagination = this.productCache.get(Object.values(this.shopParams).join('_'));
+        if(this.pagination) return of(this.pagination);
+      }
+    }
+
     let params = new HttpParams();
 
-    if(shopParams.brandId > 0) params = params.append('brandId', shopParams.brandId);
-    if(shopParams.typeId > 0) params = params.append('typeId', shopParams.typeId);
-    params = params.append('sort', shopParams.sort);
-    params = params.append('pageIndex', shopParams.pageNumber);
-    params = params.append('pageSize', shopParams.pageSize);
-    if(shopParams.search) params = params.append('search', shopParams.search);
+    if(this.shopParams.brandId > 0) params = params.append('brandId', this.shopParams.brandId);
+    if(this.shopParams.typeId > 0) params = params.append('typeId', this.shopParams.typeId);
+    params = params.append('sort', this.shopParams.sort);
+    params = params.append('pageIndex', this.shopParams.pageNumber);
+    params = params.append('pageSize', this.shopParams.pageSize);
+    if(this.shopParams.search) params = params.append('search', this.shopParams.search);
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', { params }).pipe(
       map(response => {
-        this.products = response.data;
+        this.productCache.set(Object.values(this.shopParams).join('_'), response)
+        this.pagination = response;
         return response;
       })
     );
+  }
+
+  setShopParams(params: ShopParams) {
+    this.shopParams = params;
+  }
+
+  getShopParams() {
+    return this.shopParams;
   }
 
   getBrands() {
